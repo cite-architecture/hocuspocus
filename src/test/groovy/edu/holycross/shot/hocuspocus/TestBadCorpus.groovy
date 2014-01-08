35d099b303cc1aa8bf2e632726941e5b0088721a
Neel Smith <dnsmith.neel@gmail.com>
2014-01-08 16:25:58 -0500

    Clean up badcorpus test.

--- src/test/groovy/edu/holycross/shot/hocuspocus/TestBadCorpus.groovy
+++ src/test/groovy/edu/holycross/shot/hocuspocus/TestBadCorpus.groovy
@@ -11,16 +11,17 @@ import edu.harvard.chs.cite.CtsUrn
 */
 class TestBadCorpus extends GroovyTestCase {
 
-    String testTIFile = "testdata/testCorpusTextInv.xml"
-    String testOnlineTIFile = "testdata/testOnlineCorpusTextInv.xml"
+
+    File tiFile = new File( "testdata/corpus/testinventory.xml")
+    File xml = new File("testdata/corpus/xml")
 
     /** Tests parameter requirements for constructor
     * with valid CTS TextInventory and readable text archive.
     */
     @Test void testConstructor() {
-        File inv = new File(testTIFile)
-        File dir = new File("testdata/archive")
-        Corpus c = new Corpus(inv, dir)
+
+
+        Corpus c = new Corpus(tiFile, xml)
         assert c
 
         File fake = new File("fake-inv-file")
@@ -39,54 +40,13 @@ class TestBadCorpus extends GroovyTestCase {
     */
 
     @Test void testValidator() {
-        File inv = new File(testTIFile)
-        File dir = new File("testdata/archive")
-        Corpus c = new Corpus(inv, dir)
+        Corpus c = new Corpus(tiFile, xml)
         c.validateInventory()
 
         File badinv = new File("testdata/bogusTI.xml")
         shouldFail {
-            Corpus badcorpus = new Corpus(badinv, dir)
+            Corpus badcorpus = new Corpus(badinv, xml)
         }
-    }
-
-    /** Tests contents of TextInventory against
-    * contents of archive.
-    */
-    @Test void testOnline() {
-        def expectedInvOnline = ["urn:cts:greekLit:tlg0012.tlg001.msA", "urn:cts:greekLit:tlg0012.tlg001.msB"]
-
-        def relativeBase = "testdata/archive/"
-        File inv = new File(testTIFile)
-        File dir = new File(relativeBase)
-        Corpus c = new Corpus(inv, dir)
-
-        assert  c.inventory.allOnline() == expectedInvOnline
-
-        def expectedInArchive = [ 'A_Iliad_p5uc.xml', 'tier2/B_Iliad_p5.xml'] as Set
-        def filesInArchive = c.filesInArchive() as Set
-
-        assert expectedInArchive == filesInArchive
-        assert c.filesAndInventoryMatch()
-    }
-
-
-    /** Tests methods reporting file names and URNs of documents
-    * the archive.
-    */
-    @Test void testOnlineMethods() {
-
-        File invFile = new File(testOnlineTIFile)
-        File dir = new File("testdata/testArchive")
-        Corpus c = new Corpus(invFile, dir)
-
-        def expectedSet = ["A_Iliad_testlines.xml", "B_Iliad_test2.xml", "no-namespace.xml"] as Set
-        def actualSet = c.filesInInventory() as Set
-        assert expectedSet == actualSet
-
-        def expectedUrns =  ["urn:cts:greekLit:tlg0012.tlg001.testlines", "urn:cts:greekLit:tlg0012.tlg001.test2", "urn:cts:test1:test.unclassified.unittest"] as Set
-        def actualUrns = c.urnsInInventory() as Set
-        assert actualUrns == expectedUrns
     }
 
 }
