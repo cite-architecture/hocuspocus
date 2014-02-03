7d94508fa97b2ac69cd3c0aba32778c7118b231e
Neel Smith <dnsmith.neel@gmail.com>
2014-02-03 06:06:32 +0100

    Update comments in interface for new, more explicit definition of TokenizationSystem.

--- src/main/groovy/edu/holycross/shot/hocuspocus/TokenizationSystem.groovy
+++ src/main/groovy/edu/holycross/shot/hocuspocus/TokenizationSystem.groovy
@@ -5,14 +5,16 @@ package edu.holycross.shot.hocuspocus
 */
 public interface TokenizationSystem {
 
-    /** Returns a description of the tokenization system */
+    /** Returns a human-readable description of the tokenization system */
     String getDescription()
 
 
     /** Tokenizes a tabulated representation of a text.
     * @param inputFile File with the tabular representation of the text.
     * @param separatorStr String used to separate columns in the tabular file.
-    * @returns An ordered list of two-item lists consisting of a string and a class.
+    * @returns An ordered list of URN pairs.  The first URN in each pair is a CTS
+    * URN including subreference identifying the token.  The second URN classifies
+    * the token with an appropriate value determined by the implementing system.
     */
     ArrayList tokenize(File inputFile, String separatorStr)
 
