2c84e4cb9693b25180ede74e2372f22fe8b69452
Neel Smith <dnsmith.neel@gmail.com>
2015-05-28 12:53:54 -0400

    Add unit test on namespace mapping

---
+++ src/test/groovy/edu/holycross/shot/hocuspocus/TestNsClosure.groovy
@@ -0,0 +1,28 @@
+package edu.holycross.shot.hocuspocus
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+import edu.harvard.chs.cite.TextInventory
+import edu.harvard.chs.cite.CtsUrn
+
+/**
+*/
+class TestNsClosure extends GroovyTestCase {
+
+
+  
+    void testNsMapping() {
+      Tabulator t = new Tabulator()
+
+      String expectedKey = "tei"
+      String expectedValue = "http://www.tei-c.org/ns/1.0"
+      
+      def nsMaps = t.getNSMaps()
+      // Only one predefined namespace: tei
+      assert nsMaps.size() == 1
+      assert nsMaps.keySet()[0] == expectedKey
+      assert nsMaps[expectedKey] == expectedValue
+    }
+
+}
