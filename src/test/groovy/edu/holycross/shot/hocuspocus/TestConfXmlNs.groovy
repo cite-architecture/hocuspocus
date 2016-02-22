c86a2cf80433df4dd079f6a1419d6a36a9ad6e15
Neel Smith <dnsmith.neel@gmail.com>
2016-02-22 10:31:37 +0100

    Test reading file names from conf file

--- src/test/groovy/edu/holycross/shot/hocuspocus/TestConfXmlNs.groovy
+++ src/test/groovy/edu/holycross/shot/hocuspocus/TestConfXmlNs.groovy
@@ -8,30 +8,31 @@ import static groovy.test.GroovyAssert.shouldFail
 
 
 /** */
-class TestTtlXmlNs {
+class TestConfXmlNs {
 
   @Test
   void testNsMapping() {
 
+    File confFile = new File("testdata/newconf/citationconfig3.xml")
 
-    TextInventory ti = new TextInventory(new File("testdata/testcorpus2/ti2.xml"))
-    String ttl = CtsTtl.turtleizeInv(ti, false)
-    // There are 3 texts: 1 has 1 NS mapping, 1 has 2, 1 has 0,
-    // drawn from 2 distinct XML namespaces, so:
+/*
+
+
     Integer expectedXmlNamespaces = 2
     Integer expectedMappings = 3
 
     Integer actualXmlNamespaces = 0
     Integer actualMappings = 0
     ttl.eachLine { l ->
-      if (l ==~ /.*cts:abbreviatedBy.*/) {
+      if (l ==~ /.*cts:abbreviatedBy.* /) {
           actualXmlNamespaces++
-      } else if (l ==~ /.*cts:xmlns.*/) {
+      } else if (l ==~ / . *cts:xmlns . * /) {
         actualMappings++
       }
     }
     assert actualXmlNamespaces == expectedXmlNamespaces
     assert actualMappings == expectedMappings
+    */
   }
 
 }
