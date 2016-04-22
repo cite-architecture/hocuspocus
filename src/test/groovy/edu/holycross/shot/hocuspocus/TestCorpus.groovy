ba646de1040c56c4be2fb3e07a3cdd0e9967cf36
Neel Smith <dnsmith.neel@gmail.com>
2016-04-22 12:12:45 -0400

    Check all RDF verbs in output

--- src/test/groovy/edu/holycross/shot/hocuspocus/TestCorpus.groovy
+++ src/test/groovy/edu/holycross/shot/hocuspocus/TestCorpus.groovy
@@ -35,11 +35,27 @@ class TestCorpus  {
 
 		File ttl = new File(outDir,"cts.ttl")
 
-
+    // Count occurrences of RDF verbs in output:
     def lines = 0
     def  prefix = 0
     def prev = 0
     def nxt = 0
+    def possess = 0
+    def belongs = 0
+    def dcterms = 0
+    def isPsg = 0
+    def hasPsg = 0
+    def txtContent = 0
+    def seq = 0
+    def contains = 0
+    def containedBy = 0
+    def abbr = 0
+    def ctsns = 0
+    def ctsuri = 0
+    def lang = 0
+    def ed = 0
+
+
 		ttl.eachLine { l ->
       lines++
       if (l ==~ '@prefix.+') {
@@ -48,29 +64,67 @@ class TestCorpus  {
         prev++
       } else if (l ==~ '.+cts:next.+') {
         nxt++
+
+      } else if (l ==~ '.+cts:possesses.+') {
+        possess++
+      } else if (l ==~ '.+cts:belongsTo.+') {
+        belongs++
+      } else if (l ==~ '.+dcterms:title.+') {
+        dcterms++
+      } else if (l ==~ '.+cts:isPassageOf.+') {
+        isPsg++
+      } else if (l ==~ '.+cts:hasPassage.+') {
+        hasPsg++
+      } else if (l ==~ '.+cts:hasTextContent.+') {
+        txtContent++
+      } else if (l ==~ '.+cts:hasSequence.+') {
+        seq++
+      }  else if (l ==~ '.+cts:contains.+') {
+        contains++
+      } else if (l ==~ '.+cts:containedBy.+') {
+        containedBy++
+      } else if (l ==~ '.+cts:abbreviatedBy.+') {
+        abbr++
+      } else if (l ==~ '.+cts:Namespace.+') {
+        ctsns++
+      } else if (l ==~ '.+cts:fullUri.+') {
+        ctsuri++
+      } else if (l ==~ '.+cts:Edition.+') {
+        ed++
+      } else if (l ==~ '.+cts:lang.+') {
+        lang++
       }
+
 		}
-    System.err.println "SAW " + lines + " lines."
+
     assert prefix == 4
+
+    assert possess == 3
+    assert belongs == 3
+    assert dcterms == 3
+
+    assert isPsg == 2
+    assert hasPsg == 2
+    assert seq == 2
+    assert txtContent == 2
+    assert contains == 2
+    assert containedBy == 2
 
     assert prev == 1
     assert nxt == 1
 
+    assert abbr == 1
+    assert ctsns == 1
+    assert ctsuri == 1
 
+    assert lang == 1
+    assert ed == 1
 
-		// there should be ONE cts:next and ONE cts:prev
-		// ONE abbreviatedBy, ONE cts:Namespace, ONE fullUri
-		// THREE cts:possesses ~ belongsTo, dcterms:title
-		// ONE cts:lang
-		// TWO cts:isPassageOf ~ hasPassage
-		// TWO hasSequence, hasTextContent
-		// TWO cts:containedBy ~ contains
-		// ONE cts:Edition
 
 
 
     // Clean up all files used in test:
-		ttl.delete()
+		//ttl.delete()
 		File tabDir = new File(outDir, "tabFiles")
 		tabDir.deleteDir()
 
