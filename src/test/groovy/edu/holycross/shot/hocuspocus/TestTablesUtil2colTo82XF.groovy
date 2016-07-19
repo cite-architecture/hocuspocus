626f63b2aec56db558a271ae701f87ffc9b01e37
Neel Smith <dnsmith.neel@gmail.com>
2016-07-19 13:25:28 -0400

    Force all testdata to rely on a single local copy of schemas.

--- src/test/groovy/edu/holycross/shot/hocuspocus/TestTablesUtil2colTo82XF.groovy
+++ src/test/groovy/edu/holycross/shot/hocuspocus/TestTablesUtil2colTo82XF.groovy
@@ -13,7 +13,7 @@ class TestTablesUtil2colTo82XF {
 
   @Test
   void testTablesUtilIndexSeq() {
-    File tabs = new File("testdata/2cols/achterhuis_2cols.txt")
+    File tabs = new File("testdata/2cols/archive/achterhuis_2cols.txt")
 
     TablesUtil tu = new TablesUtil()
     String str82xf = tu.twoTo82XF(tabs)
